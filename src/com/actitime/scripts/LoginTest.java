3f62979ef57e6df774ff2e67915dc7dcbf1d70de
nishi <nishimittaldec12@gmail.com>
2018-06-08 00:18:08 +0530

    Adding author name again

--- src/com/actitime/scripts/LoginTest.java
+++ src/com/actitime/scripts/LoginTest.java
@@ -1,3 +1,10 @@
+/*
+ * 
+ * @author Nishi Mittal
+ * 
+ */
+
+
 package com.actitime.scripts;
 
 import org.testng.annotations.Test;
@@ -8,27 +15,29 @@ import com.actitime.generic.WaitStatementLib;
 import com.actitime.pageobjects.EnterTimeTrackPage;
 import com.actitime.pageobjects.LoginPage;
 
-public class LoginTest extends BaseLib{
-@Test(priority=1)
-public void validLoginTest() throws InterruptedException{
-	LoginPage lp=new LoginPage(driver);
-	String username = ExcelUtils.readData("Sheet1",1,1);
-	String password = ExcelUtils.readData("Sheet1",1,2);
-	lp.login(username, password);
-	EnterTimeTrackPage ettp=new EnterTimeTrackPage(driver);
-	WaitStatementLib.isleep(2);
-	ettp.verifyHomePageTitle(driver);
-	ettp.verifyHomePageUrl(driver);
-}
-@Test(priority=2)
-public void invalidLoginTest() throws InterruptedException{
-	LoginPage lp=new LoginPage(driver);
-	String username = ExcelUtils.readData("Sheet1", 2, 1);
-	String password = ExcelUtils.readData("Sheet1", 2, 2);
-	lp.login(username, password);
-	//WaitStatementLib.explicitWaitForVisibility(driver, 10, lp.getInvalidLoginMsg());
-	WaitStatementLib.isleep(4);
-	lp.verifyInvalidLoginMsg();
-	
-}
+public class LoginTest extends BaseLib {
+	@Test(priority = 1)
+	public void validLoginTest() throws InterruptedException {
+		LoginPage lp = new LoginPage(driver);
+		String username = ExcelUtils.readData("Sheet1", 1, 1);
+		String password = ExcelUtils.readData("Sheet1", 1, 2);
+		lp.login(username, password);
+		EnterTimeTrackPage ettp = new EnterTimeTrackPage(driver);
+		WaitStatementLib.isleep(2);
+		ettp.verifyHomePageTitle(driver);
+		ettp.verifyHomePageUrl(driver);
+	}
+
+	@Test(priority = 2)
+	public void invalidLoginTest() throws InterruptedException {
+		LoginPage lp = new LoginPage(driver);
+		String username = ExcelUtils.readData("Sheet1", 2, 1);
+		String password = ExcelUtils.readData("Sheet1", 2, 2);
+		lp.login(username, password);
+		// WaitStatementLib.explicitWaitForVisibility(driver, 10,
+		// lp.getInvalidLoginMsg());
+		WaitStatementLib.isleep(4);
+		lp.verifyInvalidLoginMsg();
+
+	}
 }
